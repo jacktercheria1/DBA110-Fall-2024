b81f3ef9d4e294de066fa88af956e611f3b33930
jacktercheria1 <43801954+jacktercheria1@users.noreply.github.com>
2024-10-22 11:09:46 -0400

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,1 +1,1 @@
-SELECT * FROM Employees WHERE employee_id > 5 and email_address LIKE '%@gmail.com%'
+SELECT * FROM Employees WHERE employee_id between 5 and 10 and email_address = 'john.q.public@yahoo.com'
